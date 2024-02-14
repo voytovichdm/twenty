9eb5aee0f3b2ebf956af2bc3a7500b6e2d865699
bosiraphael <raphael.bosi@gmail.com>
2024-02-14 17:17:18 +0100

    Add job to delete messages from handle on blocklist deletion

--- packages/twenty-server/src/workspace/messaging/listeners/messaging-blocklist.listener.ts
+++ packages/twenty-server/src/workspace/messaging/listeners/messaging-blocklist.listener.ts
@@ -4,6 +4,10 @@ import { OnEvent } from '@nestjs/event-emitter';
 import { ObjectRecordDeleteEvent } from 'src/integrations/event-emitter/types/object-record-delete.event';
 import { MessageQueue } from 'src/integrations/message-queue/message-queue.constants';
 import { MessageQueueService } from 'src/integrations/message-queue/services/message-queue.service';
+import {
+  DeleteMessageFromHandleJobData,
+  DeleteMessageFromHandleJob,
+} from 'src/workspace/messaging/jobs/delete-message-from-handle.job';
 import { BlocklistObjectMetadata } from 'src/workspace/workspace-sync-metadata/standard-objects/blocklist.object-metadata';
 
 @Injectable()
@@ -16,5 +20,14 @@ export class MessagingBlocklistListener {
   @OnEvent('blocklist.deleted')
   handleDeletedEvent(
     payload: ObjectRecordDeleteEvent<BlocklistObjectMetadata>,
-  ) {}
+  ) {
+    this.messageQueueService.add<DeleteMessageFromHandleJobData>(
+      DeleteMessageFromHandleJob.name,
+      {
+        workspaceId: payload.workspaceId,
+        workspaceMemberId: payload.deletedRecord.id,
+        handle: payload.deletedRecord.handle,
+      },
+    );
+  }
 }
