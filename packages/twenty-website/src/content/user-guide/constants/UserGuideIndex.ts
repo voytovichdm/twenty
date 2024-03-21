aed3701715f618dba356911caf5ef0d4154e1692
Bonapara <tdesfrancs@gmail.com>
2024-03-21 19:18:39 +0100

    updating content and structure

--- packages/twenty-website/src/content/user-guide/constants/UserGuideIndex.ts
+++ packages/twenty-website/src/content/user-guide/constants/UserGuideIndex.ts
@@ -10,19 +10,22 @@ export type IndexHeading = {
 export const USER_GUIDE_INDEX = {
   'Getting Started': [
     { title: 'What is Twenty', url: 'what-is-twenty' },
-    { title: 'Create a Workspace', url: 'create-a-workspace' },
-    { title: 'Import your data', url: 'import-your-data' },
+    { title: 'Create a Workspace', url: 'create-workspace' },
+    { title: 'Import/Export data', url: 'import-data' },
   ],
   Objects: [
     { title: 'People', url: 'people' },
     { title: 'Companies', url: 'companies' },
     { title: 'Opportunities', url: 'opportunities' },
     { title: 'Custom Objects', url: 'custom-objects' },
-    { title: 'Remote Objects', url: 'remote-objects' },
   ],
   Functions: [
     { title: 'Emails', url: 'emails' },
     { title: 'Notes', url: 'notes' },
     { title: 'Tasks', url: 'tasks' },
+    { title: 'Integrations', url: 'integrations'},
+    { title: 'API and Webhooks', url: 'api-webhooks'},
+    { title: 'Glossary', url: 'glossary'},
+    { title: 'Tips', url: 'tips'}
   ],
 };
